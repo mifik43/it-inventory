ad4e1534cdfbda8bee744838d1439c718ca540b8
mifik43 <mif.123@mail.ru>
2025-11-12 16:05:50 +0300

    экспорт файла починил

--- wifi_utils.py
+++ wifi_utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import io
 from datetime import datetime
 from database import get_db
-from flask import send_file
+
 
 def export_guest_wifi_to_excel():
     """Экспорт данных гостевого WiFi в Excel"""
