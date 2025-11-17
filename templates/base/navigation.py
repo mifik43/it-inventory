ef73446a5877340d8e7c808e82b23ff0f67e018d
mifik43 <mif.123@mail.ru>
2025-11-17 14:50:52 +0300

    почистил апп, перенес все в папки

--- templates/base/navigation.py
+++ templates/base/navigation.py
@@ -116,7 +116,7 @@ def create_simple_menu():
     menu.add_item(MenuItem(icon="bi-check-square",name="Задачи", url="todo.todo", urls_to_be_active= ['todo.todo', 'todo.add_todo', 'todo.edit_todo'], permissions=[Permissions.todo_manage, Permissions.todo_read]))
     menu.add_item(MenuItem(icon="bi-calendar-week",name="График смен", url="shifts.shifts_list", urls_to_be_active= ['shifts.shifts_list', 'shifts.add_shift', 'shifts.edit_shift'], permissions=[Permissions.shifts_manage, Permissions.todo_read]))
     menu.add_item(MenuItem(icon="fa-network-wired",name="Сканирование сети", url="network_scan.network_scan", urls_to_be_active= ['network_scan.network_scan'], permissions=[]))
-    menu.add_item(MenuItem(icon="fa-terminal",name="Скрипты", url="scripts_list", urls_to_be_active= ['scripts_list'], permissions=[]))
+    menu.add_item(MenuItem(icon="fa-terminal",name="Скрипты", url="script.script_list", urls_to_be_active= ['script.script_list'], permissions=[]))
     menu.add_item(MenuItem(icon="bi-calendar-week",name="Роли пользователей", url="roles.roles", urls_to_be_active= ['roles.roles', 'roles.create_role', 'roles.edir_role'], permissions=[Permissions.roles_manage, Permissions.roles_read]))
     menu.add_item(MenuItem(icon="bi-people",name="Пользователи", url="users.users", urls_to_be_active= ['users.users', 'users.create_user', 'users.edit_user'], permissions=[Permissions.users_manage, Permissions.users_read]))
 
@@ -128,8 +128,8 @@ def create_menu():
     main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-pc-display", name="Устройства", url="devices.devices", urls_to_be_active= ["devices.devices", "devices.add_device", "devices.edit_device"], permissions=[Permissions.devices_manage, Permissions.devices_read]))
     main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-wifi",name="Провайдеры", url="providers.providers", urls_to_be_active= ['providers.providers', 'providers.add_provider', 'providers.edit_provider'], permissions=[Permissions.providers_manage, Permissions.providers_read]))
     main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-router",name="Гостевой WiFi", url="guest_wifi.guest_wifi", urls_to_be_active= ['guest_wifi.guest_wifi','guest_wifi.add_guest_wifi','guest_wifi.edit_guest_wifi'], permissions=[Permissions.guest_wifi_manage, Permissions.guest_wifi_read]))
-    main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-terminal",name="WTware Конфигурации", url="wtware_list", urls_to_be_active= ['wtware_list', 'add_wtware', 'edit_wtware'], permissions=[]))
-    main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-clock-history",name="История развертываний", url="wtware_deployments", urls_to_be_active= ['wtware_deployments'], permissions=[]))
+    main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-terminal",name="WTware Конфигурации", url="wtware.wtware_list", urls_to_be_active= ['wtware.wtware_list', 'add_wtware', 'edit_wtware'], permissions=[]))
+    main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-clock-history",name="История развертываний", url="wtware.wtware_deployments", urls_to_be_active= ['wtware.wtware_deployments'], permissions=[]))
     main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-box",name="Программы", url="cubes.cubes", urls_to_be_active= ['cubes.cubes', 'cubes.add_cube', 'cubes.edit_cube'], permissions=[Permissions.cubes_manage, Permissions.cubes_read]))
     main_menu.add_item(MenuItem(button_class="dropdown-item", icon="bi-building",name="Организации", url="organizations.organizations", urls_to_be_active= ['organizations.organizations', 'organizations.add_organization', 'organizations.edit_organization'], permissions=[Permissions.organizations_manage, Permissions.organizations_read]))
 
